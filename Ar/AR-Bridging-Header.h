1bf4ebef6347657ac5266333bf34d63aba9062c0
Marc-Antoine Hebert <marc_antoine20@hotmail.com>
2018-02-19 11:12:26 -0500

    Ready for demo of iteration #1

---
+++ Ar/AR-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#define HAS_LIBCXX
+#import "Structure.h"
+#import "StructureSLAM.h"
+#import <OpenGLES/gltypes.h>
+#import <MessageUI/MessageUI.h>
